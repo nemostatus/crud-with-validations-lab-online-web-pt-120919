c692033343c2a319135d82cc8ea434f53b79fe1f
Matthew <mattcorralejo1@gmail.com>
2020-05-09 05:36:50 -0700

    completed validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,11 @@
 class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: true 
+    validates :released, inclusion: {in: [true,false]}
+    validates :release_year, presence: true, if: :released, numericality: { 
+        only_integer: true, 
+        less_than_or_equal_to: ->(_brewery) { Date.current.year }
+      } 
+    validates :artist_name, presence: true 
+ 
+   
 end
